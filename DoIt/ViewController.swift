08609dc1ff7ece307c637a8291865d12b95d094b
Johan Wallin <wallinnr12@gmail.com>
2018-11-19 22:33:18 +0100

    Initial Commit

---
+++ DoIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoIt
+//
+//  Created by Johan on 2018-11-19.
+//  Copyright © 2018 Johan W. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
