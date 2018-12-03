9767fea5ff57fc1401be0908d7b9087ea8327f3d
Johan Wallin <wallinnr12@gmail.com>
2018-12-03 21:40:02 +0100

    Added 2nd viewcontroller

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Johan on 2018-12-03.
+//  Copyright © 2018 Johan W. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
